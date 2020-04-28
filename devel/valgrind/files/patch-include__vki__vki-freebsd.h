1994ca55d435986012329d474cd4da0359844130
Sean Champ <spchamp@users.noreply.github.com>
2020-04-28 02:34:16 -0700

    update sysutils/dtc, use in qemu, qemu-devel builds; update for valgrind
    devel/valgrind: Update vki-freebsd.h patch for changes recommended in
      http://bugs.freebsd.org/bugzilla/show_bug.cgi?id=239669
    
    emulators/qemu-devel: Add dtc dependency; move debug support into a
      DEBUG port option, also setting WITH_DEBUG in this situation, enabling
      the option by default; allow build with ncurses from ports, similar to
      the emulators/qemu port; Upate how extra-ldflags are provided to the
      configure script - providing the value only once, with no quote marks,
      to ensure that the system dtc is detected during configure
    
    emulators/qemu: Update in parallel to emulator/qemu for dtc from ports
      and debug support
    
    sysutils/dtc: Update to upstream release 1.6.0 - in which, the
      function fdt_check_full() is now defined differently, to a side effect
      as that the function should not be stripped from libfdt.so during normal
      pkg staging. The function is used during qemu configure, for detecting
      the presence of a system dtc. Add port option for building with yaml
      support for dtc (not enabled by default); Add port option for valgrind
      support during make(test), for amd64, i386 architectures; Add
      post-patch hacks for ensuring that the build will not fail when yaml.h
      and valgrind/memcheck.h cannot be located under wrksrc - these were
      being added to make dependencies under generated *.d files, in local
      build, should otherwise be unused in the build; Add subst vars for
      port version in distinfo, as a convenience for port maint.

--- devel/valgrind/files/patch-include__vki__vki-freebsd.h
+++ devel/valgrind/files/patch-include__vki__vki-freebsd.h
@@ -1,6 +1,18 @@
---- include/vki/vki-freebsd.h.orig	2015-01-26 16:17:32.000000000 -0400
-+++ include/vki/vki-freebsd.h	2015-08-31 04:55:29.139189000 -0300
-@@ -684,6 +684,7 @@
+--- include/vki/vki-freebsd.h.orig	2020-04-28 07:27:40.998968000 +0000
++++ include/vki/vki-freebsd.h	2020-04-28 07:41:23.465385000 +0000
+@@ -224,6 +224,11 @@
+ #define SELFMAG	4		/* magic string size */
+ #endif
+ 
++// see http://bugs.freebsd.org/bugzilla/show_bug.cgi?id=239669
++#if !defined(ELF_NOTE_GNU)
++#define ELF_NOTE_GNU "GNU"
++#endif
++
+ //----------------------------------------------------------------------
+ // From sys/syslimits.h
+ //----------------------------------------------------------------------
+@@ -714,6 +719,7 @@
  
  #define VKI_SOCK_STREAM	1
  
@@ -8,7 +20,7 @@
  #include <netinet/tcp.h>
  
  #define VKI_TCP_NODELAY  TCP_NODELAY
-@@ -1888,11 +1889,23 @@
+@@ -1946,11 +1952,23 @@
  #define VKI_I386_SET_FSBASE     8
  #define VKI_I386_GET_GSBASE     9
  #define VKI_I386_SET_GSBASE     10
